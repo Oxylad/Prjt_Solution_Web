b4104e9a656e16865157a355c3169e39d58e98e6
Lprotoo <129598474+Lprotoo@users.noreply.github.com>
2024-11-08 11:21:10 +0100

    push

---
+++ produits.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="item">
+        <img class="img" src="">
+        <p>Prix</p>
+        <button type="button">Commander</button>
+        <a href="velo.php">Plus d'infos</a>
+    </div>
+    <div class="item">
+        <img class="img" src="">
+        <p>Prix</p>
+        <button type="button">Commander</button>
+        <a href="velo.php">Plus d'infos</a>
+    </div>
+    <div class="item">
+        <img class="img" src="">
+        <p>Prix</p>
+        <button type="button">Commander</button>
+        <a href="velo.php">Plus d'infos</a>
+    </div>
+    <div class="item">
+        <img class="img" src="">
+        <p>Prix</p>
+        <button type="button">Commander</button>
+        <a href="velo.php">Plus d'infos</a>
+    </div>
+
+</body>
+</html>
