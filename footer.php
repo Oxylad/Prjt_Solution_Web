c0641ec3e274cb5c0df123ee9e67a491e7dfcc3c
TomGeff <tgeffroy@gmail.com>
2024-12-07 16:10:51 +0100

    implémentation du header et du footer
    j'ai fini ma partie

---
+++ footer.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SmartBike</title>
+    <link rel="stylesheet" href="footerStyle.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
+</head>
+<body>
+    <footer>
+        <div class="footer-img">
+            <img class="footer-img-image" src="logo.png" alt="logo" >
+        </div>
+        <div class="footer-info">
+            <div class="footer-info__element">
+                <ul class="footer-list">
+                    <li class="footer-list__item">Adresse:6 rue de Général Moutarde </li>
+                    <li class="footer-list__item footer-list__item--space">75011 Paris</li>
+                    <li class="footer-list__item">
+                        <span class="material-symbols-outlined footer-list__icon footer-list__icon--green">call</span> 06 12 34 56 78
+                    </li>
+                    <li class="footer-list__item">
+                        <span class="material-symbols-outlined footer-list__icon">mail</span>
+                        <a href="mailto:smartBike@gmail.com" class="footer-list__email">smartBike@gmail.com  </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-info__element">
+                <ul class="footer-list footer-list--horizontal ">
+                    <li class="footer-list__item"><a href=""><i class="fa fa-instagram footer-list__social"></i></a></li>
+                    <li class="footer-list__item"><a href=""><i class="fa fa-facebook footer-list__social"></i></a></li>
+                    <li class="footer-list__item"><a href=""><i class="fa fa-twitter footer-list__social"></i></a></li>
+                    <li class="footer-list__item"><a href=""><i class="fa fa-linkedin footer-list__social"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="headerStyle.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <a href="Acceuil.php">
+            <img  src="logo.png" alt="logo" class="header-logo" >
+        </a>
+        <nav class="header-nav">
+            <ul class="header-nav__list">
+                <li class="header-nav__item"><a href="NosVelos.php" class="header-nav__link">Nos vélos </a></li>
+                <li class="header-nav__item"><a href="Contact.php" class="header-nav__link">Contact</a></li>
+                <li class="header-nav__item"><a href="aPropos.php" class="header-nav__link">À propos </a></li>
+            </ul>
+        </nav>
+</header>
+</body>
+</html>
