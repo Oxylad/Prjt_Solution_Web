6769b67c6b24a996d268ee39febcc76963d5317c
Atsuko-lab <nla2.nathan@gmail.com>
2024-12-10 18:03:53 +0100

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,25 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="accueil.css" />
+    <title>Accueil Smartbike</title>
+</head>
+<body class="body">
+    <div class="logopage">
+        <h1>SMARTBIKE</h1>
+        <img id="img" src="LOGOFINALE.png">
+    </div>
+</body>
+</html>
+
+
+
+
+
+
+
---
+++ apropos.php
@@ -0,0 +1,44 @@
+<php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A propos</title>
+    <link rel="stylesheet" href="apropos.css">
+</head>
+<body>
+    <div class="container">
+
+        <div class="item">
+            <p>Trois amis, partageant une passion commune pour l'écologie et les nouvelles technologies, se retrouvèrent un week-end pour une 
+                balade en ville. Confrontés aux embouteillages et au manque d’
+                options de transport durable, ils eurent une idée : concevoir des vélos électriques modernes, écologiques et abordables.</p>
+            <img src="vélo.jpg">
+        </div>
+
+        <div class="item reverse">
+            <img src="mael.png">
+            <p>L'ingénieur Maël : Concevoir le système électrique des vélos, y compris la batterie, le moteur, et les contrôleurs. 
+                Tester les prototypes pour s’assurer de la performance, de la sécurité et de la durabilité.
+                Rechercher des technologies innovantes pour optimiser l’autonomie et réduire les coûts.
+            </p>
+        </div>
+
+        <div class="item">
+            <p>Le Designer Nathan : Concevoir l’esthétique des vélos : formes, couleurs, matériaux.
+            Travailler sur l’ergonomie pour garantir une expérience utilisateur agréable (position de conduite, confort).
+            Collaborer avec l’ingénieur pour intégrer harmonieusement les composants techniques (moteur, batterie) au design global.</p>
+            <img src="nathan.png">
+        </div>
+
+        <div class="item reverse">
+            <img src="oscar.png">
+            <p>L'expert en buisness Oscar : Développer un modèle économique viable (prix, coûts, marges).
+            Gérer les relations avec les investisseurs, fournisseurs, et partenaires.
+            Définir une stratégie marketing et de communication pour promouvoir la marque (publicité, réseaux sociaux, événements).</p>
+        </div>
+    </div>
+</body>
+</html>
