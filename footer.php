0c45221e8244dd286a60b0c60109733df0810b15
Tom geff <tgeffroy@gmail.com>
2024-12-10 18:53:01 +0100

    footer.php

--- footer.php
+++ footer.php
@@ -16,8 +16,9 @@
         <div class="footer-info">
             <div class="footer-info__element">
                 <ul class="footer-list">
-                    <li class="footer-list__item">Adresse:6 rue de Général Moutarde </li>
-                    <li class="footer-list__item footer-list__item--space">75011 Paris</li>
+                    <li class="footer-list__item">Adresse:18 Rue Louis Pouey </li>
+                    <li class="footer-list__item footer-list__item--space">92800 Puteaux</li>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3159.789869143188!2d2.2351872767954024!3d48.88719659867854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6651ce0cc18a7%3A0x90d6646df9f0147c!2sCampus%20Hep%20Paris-La%20D%C3%A9fense!5e1!3m2!1sfr!2sfr!4v1733852837073!5m2!1sfr!2sfr" width="300" height="175" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     <li class="footer-list__item">
                         <span class="material-symbols-outlined footer-list__icon footer-list__icon--green">call</span> 06 12 34 56 78
                     </li>
@@ -40,3 +41,4 @@
     </footer>
 </body>
 </html>
+
